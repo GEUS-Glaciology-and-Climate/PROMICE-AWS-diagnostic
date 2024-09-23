124d723b89d407c147f6478b31ce9cbc2ff99f90
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2024-09-23 17:12:13 +0200

    new snow height plots

---
+++ plot_compilations/snow_height_sites_toc.md
@@ -0,0 +1,204 @@
+  * [CEN](#s0-1)
+  * [CP1](#s0-2)
+  * [DY2](#s0-3)
+  * [EGP](#s0-4)
+  * [FRE](#s0-5)
+  * [HUM](#s0-6)
+  * [JAR](#s0-7)
+  * [KAN_B](#s0-8)
+  * [KAN_L](#s0-9)
+  * [KAN_M](#s0-10)
+  * [KAN_T](#s0-11)
+  * [KAN_U](#s0-12)
+  * [KPC_L](#s0-13)
+  * [KPC_U](#s0-14)
+  * [LYN_L](#s0-15)
+  * [LYN_T](#s0-16)
+  * [MIT](#s0-17)
+  * [NAE](#s0-18)
+  * [NAU](#s0-19)
+  * [NEM](#s0-20)
+  * [NSE](#s0-21)
+  * [NUK_B](#s0-22)
+  * [NUK_K](#s0-23)
+  * [NUK_L](#s0-24)
+  * [NUK_N](#s0-25)
+  * [NUK_U](#s0-26)
+  * [QAS_A](#s0-27)
+  * [QAS_L](#s0-28)
+  * [QAS_M](#s0-29)
+  * [QAS_U](#s0-30)
+  * [SCO_L](#s0-31)
+  * [SCO_U](#s0-32)
+  * [SDL](#s0-33)
+  * [SDM](#s0-34)
+  * [SER_B](#s0-35)
+  * [SWC](#s0-36)
+  * [TAS_A](#s0-37)
+  * [TAS_L](#s0-38)
+  * [TAS_U](#s0-39)
+  * [THU_L](#s0-40)
+  * [THU_L2](#s0-41)
+  * [THU_U](#s0-42)
+  * [TUN](#s0-43)
+  * [UPE_L](#s0-44)
+  * [UPE_U](#s0-45)
+  * [UWN](#s0-46)
+  * [WEG_B](#s0-47)
+  * [WEG_L](#s0-48)
+  * [ZAC_A](#s0-49)
+  * [ZAC_L](#s0-50)
+  * [ZAC_U](#s0-51)
+## <a id='s0-1' />CEN
+![CEN](../figures/snow_height/sites/CEN.png)
+ 
+## <a id='s0-2' />CP1
+![CP1](../figures/snow_height/sites/CP1.png)
+ 
+## <a id='s0-3' />DY2
+![DY2](../figures/snow_height/sites/DY2.png)
+ 
+## <a id='s0-4' />EGP
+![EGP](../figures/snow_height/sites/EGP.png)
+ 
+## <a id='s0-5' />FRE
+![FRE](../figures/snow_height/sites/FRE.png)
+ 
+## <a id='s0-6' />HUM
+![HUM](../figures/snow_height/sites/HUM.png)
+ 
+## <a id='s0-7' />JAR
+![JAR](../figures/snow_height/sites/JAR.png)
+ 
+## <a id='s0-8' />KAN_B
+![KAN_B](../figures/snow_height/sites/KAN_B.png)
+ 
+## <a id='s0-9' />KAN_L
+![KAN_L](../figures/snow_height/sites/KAN_L.png)
+ 
+## <a id='s0-10' />KAN_M
+![KAN_M](../figures/snow_height/sites/KAN_M.png)
+ 
+## <a id='s0-11' />KAN_T
+![KAN_T](../figures/snow_height/sites/KAN_T.png)
+ 
+## <a id='s0-12' />KAN_U
+![KAN_U](../figures/snow_height/sites/KAN_U.png)
+ 
+## <a id='s0-13' />KPC_L
+![KPC_L](../figures/snow_height/sites/KPC_L.png)
+ 
+## <a id='s0-14' />KPC_U
+![KPC_U](../figures/snow_height/sites/KPC_U.png)
+ 
+## <a id='s0-15' />LYN_L
+![LYN_L](../figures/snow_height/sites/LYN_L.png)
+ 
+## <a id='s0-16' />LYN_T
+![LYN_T](../figures/snow_height/sites/LYN_T.png)
+ 
+## <a id='s0-17' />MIT
+![MIT](../figures/snow_height/sites/MIT.png)
+ 
+## <a id='s0-18' />NAE
+![NAE](../figures/snow_height/sites/NAE.png)
+ 
+## <a id='s0-19' />NAU
+![NAU](../figures/snow_height/sites/NAU.png)
+ 
+## <a id='s0-20' />NEM
+![NEM](../figures/snow_height/sites/NEM.png)
+ 
+## <a id='s0-21' />NSE
+![NSE](../figures/snow_height/sites/NSE.png)
+ 
+## <a id='s0-22' />NUK_B
+![NUK_B](../figures/snow_height/sites/NUK_B.png)
+ 
+## <a id='s0-23' />NUK_K
+![NUK_K](../figures/snow_height/sites/NUK_K.png)
+ 
+## <a id='s0-24' />NUK_L
+![NUK_L](../figures/snow_height/sites/NUK_L.png)
+ 
+## <a id='s0-25' />NUK_N
+![NUK_N](../figures/snow_height/sites/NUK_N.png)
+ 
+## <a id='s0-26' />NUK_U
+![NUK_U](../figures/snow_height/sites/NUK_U.png)
+ 
+## <a id='s0-27' />QAS_A
+![QAS_A](../figures/snow_height/sites/QAS_A.png)
+ 
+## <a id='s0-28' />QAS_L
+![QAS_L](../figures/snow_height/sites/QAS_L.png)
+ 
+## <a id='s0-29' />QAS_M
+![QAS_M](../figures/snow_height/sites/QAS_M.png)
+ 
+## <a id='s0-30' />QAS_U
+![QAS_U](../figures/snow_height/sites/QAS_U.png)
+ 
+## <a id='s0-31' />SCO_L
+![SCO_L](../figures/snow_height/sites/SCO_L.png)
+ 
+## <a id='s0-32' />SCO_U
+![SCO_U](../figures/snow_height/sites/SCO_U.png)
+ 
+## <a id='s0-33' />SDL
+![SDL](../figures/snow_height/sites/SDL.png)
+ 
+## <a id='s0-34' />SDM
+![SDM](../figures/snow_height/sites/SDM.png)
+ 
+## <a id='s0-35' />SER_B
+![SER_B](../figures/snow_height/sites/SER_B.png)
+ 
+## <a id='s0-36' />SWC
+![SWC](../figures/snow_height/sites/SWC.png)
+ 
+## <a id='s0-37' />TAS_A
+![TAS_A](../figures/snow_height/sites/TAS_A.png)
+ 
+## <a id='s0-38' />TAS_L
+![TAS_L](../figures/snow_height/sites/TAS_L.png)
+ 
+## <a id='s0-39' />TAS_U
+![TAS_U](../figures/snow_height/sites/TAS_U.png)
+ 
+## <a id='s0-40' />THU_L
+![THU_L](../figures/snow_height/sites/THU_L.png)
+ 
+## <a id='s0-41' />THU_L2
+![THU_L2](../figures/snow_height/sites/THU_L2.png)
+ 
+## <a id='s0-42' />THU_U
+![THU_U](../figures/snow_height/sites/THU_U.png)
+ 
+## <a id='s0-43' />TUN
+![TUN](../figures/snow_height/sites/TUN.png)
+ 
+## <a id='s0-44' />UPE_L
+![UPE_L](../figures/snow_height/sites/UPE_L.png)
+ 
+## <a id='s0-45' />UPE_U
+![UPE_U](../figures/snow_height/sites/UPE_U.png)
+ 
+## <a id='s0-46' />UWN
+![UWN](../figures/snow_height/sites/UWN.png)
+ 
+## <a id='s0-47' />WEG_B
+![WEG_B](../figures/snow_height/sites/WEG_B.png)
+ 
+## <a id='s0-48' />WEG_L
+![WEG_L](../figures/snow_height/sites/WEG_L.png)
+ 
+## <a id='s0-49' />ZAC_A
+![ZAC_A](../figures/snow_height/sites/ZAC_A.png)
+ 
+## <a id='s0-50' />ZAC_L
+![ZAC_L](../figures/snow_height/sites/ZAC_L.png)
+ 
+## <a id='s0-51' />ZAC_U
+![ZAC_U](../figures/snow_height/sites/ZAC_U.png)
+ 
--- plot_surface_heights_thermistor.py
+++ plot_surface_heights_thermistor.py
@@ -78,7 +78,7 @@ for station in df_meta.index:
                        label=var)
     if 't_i_10m' in df_new.columns:
         ax_list[3].plot(df_new.index, df_new['t_i_10m'].values, 
-                marker='o',markeredgecolor='None', linestyle='None', 
+                marker='o',color='k',markeredgecolor='None', linestyle='None', 
                label='t_i_10m')  
                 
     for i in range(4):
@@ -195,3 +195,127 @@ fig.supylabel('Surface height relative to installation (m)')
 
 # Save the figure
 fig.savefig('figures/surface_height/overview.png', dpi=300)
+
+# %% 
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import matplotlib
+import matplotlib.dates as mdates
+from datetime import datetime
+matplotlib.use('Agg')
+import tocgen
+import numpy as np
+
+# Initialize
+data_type = 'sites'
+path_new = '../aws-l3-dev/' + data_type + '/'
+filename = 'plot_compilations/snow_height_' + data_type + '.md'
+df_meta = pd.read_csv(path_new + '../AWS_' + data_type + '_metadata.csv')
+df_meta = df_meta.set_index(data_type[:-1] + '_id')
+f = open(filename, "w")
+
+def Msg(txt):
+    with open(filename, "a") as f:
+        print(txt)
+        f.write(txt + "\n")
+
+plt.close('all')
+
+# Loop over each station
+for station in df_meta.index:
+    Msg('## ' + station)
+    
+    # Check if the file exists
+    if not os.path.isfile(path_new + station + '/' + station + '_day.csv'):
+        continue
+    
+    # Read the station data
+    df_new = pd.read_csv(path_new + station + '/' + station + '_day.csv')
+    df_new.time = pd.to_datetime(df_new.time)
+    df_new = df_new.set_index('time')
+    
+    # Create a figure with two panels
+    fig, ax_list = plt.subplots(2, 1, sharex=False, figsize=(10, 10))
+    plt.subplots_adjust(right=0.75, left=0.08, hspace=0.15)
+    
+    # Top panel: z_surf_combined
+    if 'z_surf_combined' in df_new.columns and not df_new['z_surf_combined'].isnull().all():
+        ax_list[0].plot(df_new.index, df_new['z_surf_combined'], 
+                         marker='.', markeredgecolor='None', linestyle='None', label='z_surf_combined')
+        ax_list[0].set_ylabel('Surface Height (m)')
+        ax_list[0].set_title(station)
+        ax_list[0].grid()
+        ax_list[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    
+    # Bottom panel: snow_height as function of day of year
+    if 'snow_height' in df_new.columns and not df_new['snow_height'].isnull().all():
+        for year in df_new.index.year.unique():
+            # Filter data for the year range September to August Y
+            start_date = pd.Timestamp(f'{year-1}-09-01')
+            end_date = pd.Timestamp(f'{year}-08-31')
+            df_year = df_new.loc[(df_new.index >= start_date) & (df_new.index < end_date), :].copy()
+            
+            # Calculate the day of the year
+            df_year['day_of_year'] = df_year.index.dayofyear.values - 244
+            df_year.loc[df_year['day_of_year'] < 0, 'day_of_year'] = 365 + df_year.loc[df_year['day_of_year'] < 0, 'day_of_year']
+
+            # Plot snow_height adjusted by first valid value
+            if df_year['snow_height'].notnull().any():
+                first_valid_value = df_year['snow_height'].loc[
+                    slice(df_year['snow_height'].first_valid_index(), 
+                          df_year['snow_height'].first_valid_index()+pd.to_timedelta('10 days'))].min()
+                if year == 2024:
+                    ax_list[1].plot(df_year['day_of_year'], 
+                                     df_year['snow_height'] - first_valid_value, 
+                                     label='_no_legend_', linestyle='-', color='w',lw=4, alpha=0.5)
+                    ax_list[1].plot(df_year['day_of_year'], 
+                                     df_year['snow_height'] - first_valid_value, 
+                                     label=str(year), linestyle='-', color='k',lw=2)
+                else:
+                    ax_list[1].plot(df_year['day_of_year'], 
+                                     df_year['snow_height'] - first_valid_value, 
+                                     label=str(year), linestyle='-')
+        
+        # Set x-axis limits
+        ax_list[1].set_xlim(1, 365)  # Adjust for leap years if necessary
+        
+        # Major ticks and labels every 30 days
+        labels = []
+        tick_positions = []
+        for i in np.cumsum(np.array([30, 31, 30, 31, 31, 29, 31, 30, 31, 30, 31, 31])):
+            tick_positions.append(i)
+            # Determine the corresponding month and year for the label
+            month_label = ''
+            if i < 30: month_label = 'Sept.'
+            elif i < 60: month_label = 'Oct.'
+            elif i < 90: month_label = 'Nov.'
+            elif i < 120: month_label = 'Dec.'
+            elif i < 150: month_label = 'Jan.'
+            elif i < 180: month_label = 'Feb.'
+            elif i < 210: month_label = 'Mar.'
+            elif i < 240: month_label = 'Apr.'
+            elif i < 270: month_label = 'May'
+            elif i < 300: month_label = 'Jun.'
+            elif i < 330: month_label = 'Jul.'
+            elif i < 360: month_label = 'Aug.'
+            labels.append(month_label)
+        
+        ax_list[1].set_xticks(tick_positions)
+        ax_list[1].set_xticklabels(labels, rotation=45, ha='right')
+        
+        # Add vertical line at day 121
+        # ax_list[1].axvline(x=121, color='red', linestyle='--', label='Day 121')
+        
+        ax_list[1].set_ylabel('Snow Height (m)')
+        ax_list[1].grid()
+        ax_list[1].legend(title='Year', loc='center left', bbox_to_anchor=(1, 0.5), ncol=1)
+    
+    # Save the figure
+    fig.savefig('figures/snow_height/%s/%s.png' % (data_type, station), dpi=300)
+    Msg(f'![{station}](../figures/snow_height/{data_type}/{station}.png)')
+    Msg(' ')
+
+tocgen.processFile(filename, filename[:-3] + "_toc.md")
+f.close()
+
