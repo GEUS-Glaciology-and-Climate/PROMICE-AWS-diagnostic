c66961afed4319272d778723db9388071e0bcc8f
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2024-11-04 13:41:53 +0100

    new plots highlighting 2024-2025

--- plot_surface_heights_thermistor.py
+++ plot_surface_heights_thermistor.py
@@ -36,7 +36,7 @@ for station in df_meta.index:
     df_new = pd.read_csv(path_new+station+'/'+station+'_day.csv')
     df_new.time = pd.to_datetime(df_new.time, utc=True)
     df_new = df_new.set_index('time')
-    
+
     fig, ax_list = plt.subplots(4,1,sharex=True, figsize=(10,15))
     plt.subplots_adjust(right=0.75,left=0.08,hspace=0.02)
 
@@ -44,51 +44,51 @@ for station in df_meta.index:
                       if v in df_new.columns]
     for var in var_list:
         if var in df_new.columns:
-            if not df_new[var].isnull().all():                
-                ax_list[0].plot(df_new.index, df_new[var].values, 
-                        marker='.',markeredgecolor='None', linestyle='None', 
-                        label=var)  
-                
+            if not df_new[var].isnull().all():
+                ax_list[0].plot(df_new.index, df_new[var].values,
+                        marker='.',markeredgecolor='None', linestyle='None',
+                        label=var)
+
     var_list = [v for v in ['z_surf_combined','z_ice_surf','snow_height'] \
-                      if v in df_new.columns]       
+                      if v in df_new.columns]
     for var in var_list:
         if var in df_new.columns:
-            if not df_new[var].isnull().all():                
-                ax_list[1].plot(df_new.index, df_new[var].values, 
-                        marker='.',markeredgecolor='None', linestyle='None', 
-                       label=var)  
+            if not df_new[var].isnull().all():
+                ax_list[1].plot(df_new.index, df_new[var].values,
+                        marker='.',markeredgecolor='None', linestyle='None',
+                       label=var)
             else:
                 print(var,'all nan')
     var_list = ['d_t_i_'+str(i) for i in range(1,12) \
-                      if 'd_t_i_'+str(i) in df_new.columns]       
+                      if 'd_t_i_'+str(i) in df_new.columns]
     for var in var_list:
         if var in df_new.columns:
-            if not df_new[var].isnull().all():                
-                ax_list[2].plot(df_new.index, df_new[var].values, 
-                        marker='.',markeredgecolor='None', linestyle='None', 
-                       label=var)  
-                
+            if not df_new[var].isnull().all():
+                ax_list[2].plot(df_new.index, df_new[var].values,
+                        marker='.',markeredgecolor='None', linestyle='None',
+                       label=var)
+
     var_list = ['t_i_'+str(i) for i in range(1,12) \
-                      if 't_i_'+str(i) in df_new.columns]   
+                      if 't_i_'+str(i) in df_new.columns]
     for var in var_list:
         if var in df_new.columns:
-            if not df_new[var].isnull().all():                
-                ax_list[3].plot(df_new.index, df_new[var].values, 
-                        marker='.',markeredgecolor='None', linestyle='None', 
+            if not df_new[var].isnull().all():
+                ax_list[3].plot(df_new.index, df_new[var].values,
+                        marker='.',markeredgecolor='None', linestyle='None',
                        label=var)
     if 't_i_10m' in df_new.columns:
-        ax_list[3].plot(df_new.index, df_new['t_i_10m'].values, 
-                marker='o',color='k',markeredgecolor='None', linestyle='None', 
-               label='t_i_10m')  
-                
-    for i in range(4):
-        ax_list[i].set_ylabel('Height (m)')       
-        ax_list[i].grid()
-        ax_list[i].legend(loc='center left', bbox_to_anchor=(1, 0.5))  
-        if i > 1:
-            ax_list[i].legend(loc='center left', bbox_to_anchor=(1, 0.5),ncols=2)  
+        ax_list[3].plot(df_new.index, df_new['t_i_10m'].values,
+                marker='o',color='k',markeredgecolor='None', linestyle='None',
+               label='t_i_10m')
 
-    ax_list[2].set_ylabel('Depth (m)')       
+    for i in range(4):
+        ax_list[i].set_ylabel('Height (m)')
+        ax_list[i].grid()
+        ax_list[i].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if i > 1:
+            ax_list[i].legend(loc='center left', bbox_to_anchor=(1, 0.5),ncols=2)
+
+    ax_list[2].set_ylabel('Depth (m)')
     ax_list[2].invert_yaxis()
     ax_list[3].set_ylabel('Temperature (°C)')
     ax_list[0].set_title(station)
@@ -98,7 +98,7 @@ for station in df_meta.index:
 tocgen.processFile(filename, filename[:-3]+"_toc.md")
 f.close()
 
-# %% 
+# %%
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -153,7 +153,7 @@ for i, station_list in enumerate(station_list_list[:9]):  # Limit to the first 9
     ax_list[i].tick_params(axis='x', rotation=45)
     if i <6:
         ax_list[i].set_xticklabels([])
-        
+
 
 
     ax_list[i].set_ylim(-100,20)
@@ -196,7 +196,7 @@ fig.supylabel('Surface height relative to installation (m)')
 # Save the figure
 fig.savefig('figures/surface_height/overview.png', dpi=300)
 
-# %% 
+# %%
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
@@ -226,37 +226,37 @@ plt.close('all')
 for station in df_meta.index:
 # for station in ['QAS_U']:
     Msg('## ' + station)
-    
+
     # Check if the file exists
     if not os.path.isfile(path_new + station + '/' + station + '_day.csv'):
         continue
-    
+
     # Read the station data
     df_new = pd.read_csv(path_new + station + '/' + station + '_day.csv')
     df_new.time = pd.to_datetime(df_new.time)
     df_new = df_new.set_index('time')
-    
+
     # Create a figure with two panels
     fig, ax_list = plt.subplots(2, 1, sharex=False, figsize=(10, 10))
     plt.subplots_adjust(right=0.75, left=0.08, hspace=0.15)
-    
+
     # Top panel: z_surf_combined
     if 'z_surf_combined' in df_new.columns and not df_new['z_surf_combined'].isnull().all():
-        ax_list[0].plot(df_new.index, df_new['z_surf_combined'], 
+        ax_list[0].plot(df_new.index, df_new['z_surf_combined'],
                          marker='.', markeredgecolor='None', linestyle='None', label='z_surf_combined')
         ax_list[0].set_ylabel('Surface Height (m)')
         ax_list[0].set_title(station)
         ax_list[0].grid()
         ax_list[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    
+
     # Bottom panel: snow_height as function of day of year
     if 'snow_height' in df_new.columns and not df_new['snow_height'].isnull().all():
-        for year in df_new.index.year.unique():
+        for year in df_new.index.year.unique().tolist() + [df_new.index.year.max() + 1]:
             # Filter data for the year range September to August Y
             start_date = pd.Timestamp(f'{year-1}-09-01')
             end_date = pd.Timestamp(f'{year}-08-31')
             df_year = df_new.loc[(df_new.index >= start_date) & (df_new.index < end_date), :].copy()
-            
+
             # Calculate the day of the year
             df_year['day_of_year'] = df_year.index.dayofyear.values - 244
             df_year.loc[df_year['day_of_year'] < 0, 'day_of_year'] = 365 + df_year.loc[df_year['day_of_year'] < 0, 'day_of_year']
@@ -264,23 +264,23 @@ for station in df_meta.index:
             # Plot snow_height adjusted by first valid value
             if df_year['snow_height'].notnull().any():
                 first_valid_value = df_year['snow_height'].loc[
-                    slice(df_year['snow_height'].first_valid_index(), 
+                    slice(df_year['snow_height'].first_valid_index(),
                           df_year['snow_height'].first_valid_index()+pd.to_timedelta('30 days'))].min()
-                if year == 2024:
-                    ax_list[1].plot(df_year['day_of_year'], 
-                                     df_year['snow_height'] - first_valid_value, 
+                if year == 2025:
+                    ax_list[1].plot(df_year['day_of_year'],
+                                     df_year['snow_height'] - first_valid_value,
                                      label='_no_legend_', linestyle='-', color='w',lw=6, alpha=0.7)
-                    ax_list[1].plot(df_year['day_of_year'], 
-                                     df_year['snow_height'] - first_valid_value, 
+                    ax_list[1].plot(df_year['day_of_year'],
+                                     df_year['snow_height'] - first_valid_value,
                                      label=str(year), linestyle='-', color='k',lw=2)
                 else:
-                    ax_list[1].plot(df_year['day_of_year'], 
-                                     df_year['snow_height'] - first_valid_value, 
+                    ax_list[1].plot(df_year['day_of_year'],
+                                     df_year['snow_height'] - first_valid_value,
                                      label=str(year), linestyle='-')
-        
+
         # Set x-axis limits
         ax_list[1].set_xlim(1, 365)  # Adjust for leap years if necessary
-        
+
         # Major ticks and labels every 30 days
         labels = []
         tick_positions = []
@@ -301,17 +301,17 @@ for station in df_meta.index:
             elif i < 330: month_label = 'Jul.'
             elif i < 360: month_label = 'Aug.'
             labels.append(month_label)
-        
+
         ax_list[1].set_xticks(tick_positions)
         ax_list[1].set_xticklabels(labels, rotation=45, ha='right')
-        
+
         # Add vertical line at day 121
         # ax_list[1].axvline(x=121, color='red', linestyle='--', label='Day 121')
-        
+
         ax_list[1].set_ylabel('Snow Height (m)')
         ax_list[1].grid()
         ax_list[1].legend(title='Year', loc='center left', bbox_to_anchor=(1, 0.5), ncol=1)
-    
+
     # Save the figure
     fig.savefig('figures/snow_height/%s/%s.png' % (data_type, station), dpi=300)
     Msg(f'![{station}](../figures/snow_height/{data_type}/{station}.png)')
@@ -319,4 +319,3 @@ for station in df_meta.index:
 
 tocgen.processFile(filename, filename[:-3] + "_toc.md")
 f.close()
-
