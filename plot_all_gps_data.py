4a617f7f4691f07862b460af7d5f828e0bc3653e
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2024-12-05 11:28:06 +0100

    run GPS visu

--- plot_all_gps_data.py
+++ plot_all_gps_data.py
@@ -21,10 +21,13 @@ import geopandas as gpd
 
 # def main(
 data_type = 'sites'
-path_new = '../aws-l3-dev/'+data_type+'/'
+if data_type == 'sites':
+    path_new = 'C:/Users/bav/Downloads/level_3_sites/day/'
+else:
+    path_new = 'C:/Users/bav/Downloads/level_2_stations/day/'
+
 filename = 'plot_compilations/GPS_'+data_type+'.md'
-df_meta = pd.read_csv(path_new+'../AWS_'+data_type+'_metadata.csv')
-df_meta = df_meta.set_index(data_type[:-1]+'_id')
+
 f = open(filename, "w")
 def Msg(txt):
     f = open(filename, "a")
@@ -39,31 +42,34 @@ gps_obs = pd.read_csv('ancil/GC-Net_observed_coordinates.csv').set_index('name')
 gps_obs['date'] = pd.to_datetime(gps_obs.date,errors='coerce')
 for v in ['lat','lon','elev']:
     gps_obs[v] = pd.to_numeric(gps_obs[v], errors='coerce')
-for station in df_meta.index:
+for file in os.listdir(path_new):
 # for station in ['JAR']:
 # for station in np.unique(gnss_df.index):
-    # Msg('## '+station)
-    if not os.path.isfile(path_new+station+'/'+station+'_day.csv'):
+    station = file.replace('_day.csv','')
+    Msg('## '+station)
+    if not os.path.isfile(path_new+file):
+        Msg("cannot find",path_new+file)
         continue
-    df_new = pd.read_csv(path_new+station+'/'+station+'_day.csv')
+
+    df_new = pd.read_csv(path_new+file)
     df_new.time = pd.to_datetime(df_new.time, utc=True)
     df_new = df_new.set_index('time')
-    
+
     var_list_list = [['gps_lat','gps_lon','gps_alt']]
     for k, var_list in enumerate(var_list_list):
         fig, ax_list = plt.subplots(len(var_list),1,sharex=True, figsize=(12,8))
         plt.subplots_adjust(right=0.75,left=0.08)
         if len(var_list)==1:
             ax_list = [ax_list]
-        
+
         for var, ax in zip(var_list, ax_list):
             if var in df_new.columns:
-                if not df_new[var].isnull().all():                
-                    ax.plot(df_new.index, df_new[var].values, 
-                            marker='.',markeredgecolor='None', linestyle='None', 
-                            color='tab:orange',label=var)  
+                if not df_new[var].isnull().all():
+                    ax.plot(df_new.index, df_new[var].values,
+                            marker='.',markeredgecolor='None', linestyle='None',
+                            color='tab:orange',label=var)
             if var.replace('gps_','') in df_new.columns:
-                ax.plot(df_new.index, df_new[var.replace('gps_','')].values, 
+                ax.plot(df_new.index, df_new[var.replace('gps_','')].values,
                         color='tab:green', label=var.replace('gps_',''))
             if station in gnss_df.index:
                 if var == 'gps_alt':
@@ -71,7 +77,7 @@ for station in df_meta.index:
                             gnss_df.loc[station,var.replace('gps_','').replace('alt','orthometric_height_m')],
                             marker='o',ls='None', label='GNSS survey: orthometric height')
                     ax.plot(gnss_df.loc[station,'date'],
-                            gnss_df.loc[station,var.replace('gps_','').replace('alt','ellipsoid_height_m')], 
+                            gnss_df.loc[station,var.replace('gps_','').replace('alt','ellipsoid_height_m')],
                             marker='^',ls='None', color='tab:green', label='GNSS survey: ellipsoid height')
                 else:
                     ax.plot(gnss_df.loc[station,'date'],
@@ -81,9 +87,9 @@ for station in df_meta.index:
                 ax.plot(gps_obs.loc[station,'date'],
                         gps_obs.loc[station,var.replace('gps_','').replace('alt','elev')],
                         marker='d',color='tab:red',ls='None', label='handheld survey')
-            ax.set_ylabel(var.replace('gps_',''))       
+            ax.set_ylabel(var.replace('gps_',''))
             ax.grid()
-            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))        
+            ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         no_save = 1
         for ax in ax_list:
             if ax.lines: no_save=0
@@ -92,24 +98,24 @@ for station in df_meta.index:
             continue
         plt.suptitle(station)
         fig.savefig('figures/GPS/%s/%s_%i.png'%(data_type, station,k), dpi=300)
-        # Msg('![%s](../figures/GPS/%s/%s_%i.png)'%(station,data_type, station,k))
+        Msg('![%s](../figures/GPS/%s/%s_%i.png)'%(station,data_type, station,k))
 
         # determination of the range of displacement
         # gdf = gpd.GeoDataFrame(df_new,
-        #                        geometry=gpd.points_from_xy(df_new['lon'], 
-        #                                                    df_new['lat']), 
+        #                        geometry=gpd.points_from_xy(df_new['lon'],
+        #                                                    df_new['lat']),
         #                        crs="EPSG:4326")
         # gdf_3413 = gdf.to_crs(epsg=3413)
         # radius_km = (gdf_3413.union_all()
         #              .convex_hull.centroid
         #              .hausdorff_distance(gdf_3413.union_all())) / 1000
-        
+
         # print(station,',', round(radius_km*2, 1))
 
-    df_m = pd.read_csv(path_new+station+'/'+station+'_month.csv')
-    df_m.time = pd.to_datetime(df_m.time, utc=True)
-    df_m = df_m.set_index('time')
-    df_m[[v for v in ['lat','lon','alt'] if v in df_m.columns]].to_csv('figures/GPS/coordinates_'+data_type+'/%s.csv'%station)
-    # Msg(' ')
+    # df_m = pd.read_csv(path_new+station+'/'+station+'_month.csv')
+    # df_m.time = pd.to_datetime(df_m.time, utc=True)
+    # df_m = df_m.set_index('time')
+    # df_m[[v for v in ['lat','lon','alt'] if v in df_m.columns]].to_csv('figures/GPS/coordinates_'+data_type+'/%s.csv'%station)
+    Msg(' ')
 tocgen.processFile(filename, filename[:-3]+"_toc.md")
 f.close()
---
+++ plot_compilations/GPS_sites_toc.md
@@ -0,0 +1,216 @@
+  * [CEN](#s0-1)
+  * [CP1](#s0-2)
+  * [DY2](#s0-3)
+  * [EGP](#s0-4)
+  * [FRE](#s0-5)
+  * [HUM](#s0-6)
+  * [JAR](#s0-7)
+  * [KAN_B](#s0-8)
+  * [KAN_L](#s0-9)
+  * [KAN_M](#s0-10)
+  * [KAN_T](#s0-11)
+  * [KAN_U](#s0-12)
+  * [KPC_L](#s0-13)
+  * [KPC_U](#s0-14)
+  * [LYN_L](#s0-15)
+  * [LYN_T](#s0-16)
+  * [MIT](#s0-17)
+  * [NAE](#s0-18)
+  * [NAU](#s0-19)
+  * [NEM](#s0-20)
+  * [NSE](#s0-21)
+  * [NUK_B](#s0-22)
+  * [NUK_K](#s0-23)
+  * [NUK_L](#s0-24)
+  * [NUK_N](#s0-25)
+  * [NUK_P](#s0-26)
+  * [NUK_U](#s0-27)
+  * [ORO](#s0-28)
+  * [QAS_A](#s0-29)
+  * [QAS_L](#s0-30)
+  * [QAS_M](#s0-31)
+  * [QAS_U](#s0-32)
+  * [RED_L](#s0-33)
+  * [SCO_L](#s0-34)
+  * [SCO_U](#s0-35)
+  * [SDL](#s0-36)
+  * [SDM](#s0-37)
+  * [SER_B](#s0-38)
+  * [SWC](#s0-39)
+  * [TAS_A](#s0-40)
+  * [TAS_L](#s0-41)
+  * [TAS_U](#s0-42)
+  * [THU_L2](#s0-43)
+  * [THU_L](#s0-44)
+  * [THU_U](#s0-45)
+  * [TUN](#s0-46)
+  * [UPE_L](#s0-47)
+  * [UPE_U](#s0-48)
+  * [UWN](#s0-49)
+  * [WEG_B](#s0-50)
+  * [WEG_L](#s0-51)
+  * [ZAC_A](#s0-52)
+  * [ZAC_L](#s0-53)
+  * [ZAC_U](#s0-54)
+## <a id='s0-1' />CEN
+![CEN](../figures/GPS/sites/CEN_0.png)
+ 
+## <a id='s0-2' />CP1
+![CP1](../figures/GPS/sites/CP1_0.png)
+ 
+## <a id='s0-3' />DY2
+![DY2](../figures/GPS/sites/DY2_0.png)
+ 
+## <a id='s0-4' />EGP
+![EGP](../figures/GPS/sites/EGP_0.png)
+ 
+## <a id='s0-5' />FRE
+![FRE](../figures/GPS/sites/FRE_0.png)
+ 
+## <a id='s0-6' />HUM
+![HUM](../figures/GPS/sites/HUM_0.png)
+ 
+## <a id='s0-7' />JAR
+![JAR](../figures/GPS/sites/JAR_0.png)
+ 
+## <a id='s0-8' />KAN_B
+![KAN_B](../figures/GPS/sites/KAN_B_0.png)
+ 
+## <a id='s0-9' />KAN_L
+![KAN_L](../figures/GPS/sites/KAN_L_0.png)
+ 
+## <a id='s0-10' />KAN_M
+![KAN_M](../figures/GPS/sites/KAN_M_0.png)
+ 
+## <a id='s0-11' />KAN_T
+![KAN_T](../figures/GPS/sites/KAN_T_0.png)
+ 
+## <a id='s0-12' />KAN_U
+![KAN_U](../figures/GPS/sites/KAN_U_0.png)
+ 
+## <a id='s0-13' />KPC_L
+![KPC_L](../figures/GPS/sites/KPC_L_0.png)
+ 
+## <a id='s0-14' />KPC_U
+![KPC_U](../figures/GPS/sites/KPC_U_0.png)
+ 
+## <a id='s0-15' />LYN_L
+![LYN_L](../figures/GPS/sites/LYN_L_0.png)
+ 
+## <a id='s0-16' />LYN_T
+![LYN_T](../figures/GPS/sites/LYN_T_0.png)
+ 
+## <a id='s0-17' />MIT
+![MIT](../figures/GPS/sites/MIT_0.png)
+ 
+## <a id='s0-18' />NAE
+![NAE](../figures/GPS/sites/NAE_0.png)
+ 
+## <a id='s0-19' />NAU
+![NAU](../figures/GPS/sites/NAU_0.png)
+ 
+## <a id='s0-20' />NEM
+![NEM](../figures/GPS/sites/NEM_0.png)
+ 
+## <a id='s0-21' />NSE
+![NSE](../figures/GPS/sites/NSE_0.png)
+ 
+## <a id='s0-22' />NUK_B
+![NUK_B](../figures/GPS/sites/NUK_B_0.png)
+ 
+## <a id='s0-23' />NUK_K
+![NUK_K](../figures/GPS/sites/NUK_K_0.png)
+ 
+## <a id='s0-24' />NUK_L
+![NUK_L](../figures/GPS/sites/NUK_L_0.png)
+ 
+## <a id='s0-25' />NUK_N
+![NUK_N](../figures/GPS/sites/NUK_N_0.png)
+ 
+## <a id='s0-26' />NUK_P
+![NUK_P](../figures/GPS/sites/NUK_P_0.png)
+ 
+## <a id='s0-27' />NUK_U
+![NUK_U](../figures/GPS/sites/NUK_U_0.png)
+ 
+## <a id='s0-28' />ORO
+![ORO](../figures/GPS/sites/ORO_0.png)
+ 
+## <a id='s0-29' />QAS_A
+![QAS_A](../figures/GPS/sites/QAS_A_0.png)
+ 
+## <a id='s0-30' />QAS_L
+![QAS_L](../figures/GPS/sites/QAS_L_0.png)
+ 
+## <a id='s0-31' />QAS_M
+![QAS_M](../figures/GPS/sites/QAS_M_0.png)
+ 
+## <a id='s0-32' />QAS_U
+![QAS_U](../figures/GPS/sites/QAS_U_0.png)
+ 
+## <a id='s0-33' />RED_L
+![RED_L](../figures/GPS/sites/RED_L_0.png)
+ 
+## <a id='s0-34' />SCO_L
+![SCO_L](../figures/GPS/sites/SCO_L_0.png)
+ 
+## <a id='s0-35' />SCO_U
+![SCO_U](../figures/GPS/sites/SCO_U_0.png)
+ 
+## <a id='s0-36' />SDL
+![SDL](../figures/GPS/sites/SDL_0.png)
+ 
+## <a id='s0-37' />SDM
+![SDM](../figures/GPS/sites/SDM_0.png)
+ 
+## <a id='s0-38' />SER_B
+![SER_B](../figures/GPS/sites/SER_B_0.png)
+ 
+## <a id='s0-39' />SWC
+![SWC](../figures/GPS/sites/SWC_0.png)
+ 
+## <a id='s0-40' />TAS_A
+![TAS_A](../figures/GPS/sites/TAS_A_0.png)
+ 
+## <a id='s0-41' />TAS_L
+![TAS_L](../figures/GPS/sites/TAS_L_0.png)
+ 
+## <a id='s0-42' />TAS_U
+![TAS_U](../figures/GPS/sites/TAS_U_0.png)
+ 
+## <a id='s0-43' />THU_L2
+![THU_L2](../figures/GPS/sites/THU_L2_0.png)
+ 
+## <a id='s0-44' />THU_L
+![THU_L](../figures/GPS/sites/THU_L_0.png)
+ 
+## <a id='s0-45' />THU_U
+![THU_U](../figures/GPS/sites/THU_U_0.png)
+ 
+## <a id='s0-46' />TUN
+![TUN](../figures/GPS/sites/TUN_0.png)
+ 
+## <a id='s0-47' />UPE_L
+![UPE_L](../figures/GPS/sites/UPE_L_0.png)
+ 
+## <a id='s0-48' />UPE_U
+![UPE_U](../figures/GPS/sites/UPE_U_0.png)
+ 
+## <a id='s0-49' />UWN
+![UWN](../figures/GPS/sites/UWN_0.png)
+ 
+## <a id='s0-50' />WEG_B
+![WEG_B](../figures/GPS/sites/WEG_B_0.png)
+ 
+## <a id='s0-51' />WEG_L
+![WEG_L](../figures/GPS/sites/WEG_L_0.png)
+ 
+## <a id='s0-52' />ZAC_A
+![ZAC_A](../figures/GPS/sites/ZAC_A_0.png)
+ 
+## <a id='s0-53' />ZAC_L
+![ZAC_L](../figures/GPS/sites/ZAC_L_0.png)
+ 
+## <a id='s0-54' />ZAC_U
+![ZAC_U](../figures/GPS/sites/ZAC_U_0.png)
+ 
