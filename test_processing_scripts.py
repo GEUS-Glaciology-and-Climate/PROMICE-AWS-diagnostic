e5ef1202db4bb6247f5076ac4fb8b94180c4cca3
Baptiste Vandecrux <35140661+BaptisteVandecrux@users.noreply.github.com>
2025-11-26 12:18:50 +0100

    new plots

--- test_processing_scripts.py
+++ test_processing_scripts.py
@@ -99,17 +99,20 @@ def get_join_l3(site):
             os.remove(f)
 
     l3_merged, sorted_list_station_data = join_l3(config_folder, site, path_l3_stations,
-                        folder_gcnet, 'L3_test/sites/', None, None)
+                        folder_gcnet, folder_glaciobasis, 'L3_test/sites/', None, None)
     return l3_merged, sorted_list_station_data
 
 
 if __name__ == '__main__':
     df_metadata = pd.read_csv('../thredds-data/metadata/AWS_stations_metadata.csv')
-    for station in ['DY2']:
+    # for station in ['TUN']:
     # for station in np.unique(np.array(df_metadata.station_id)):
-        pAWS_tx, pAWS_raw, l2_merged, l3 = process_l2_l3(station)
+        # pAWS_tx, pAWS_raw, l2_merged, l3 = process_l2_l3(station)
 
     df_metadata = pd.read_csv('../thredds-data/metadata/AWS_sites_metadata.csv')
-    for site in ['FRE']:
+    for site in ['TUN']:
     # for site in df_metadata.site_id:
         l3_merged, sorted_list_station_data = get_join_l3(site)
+
+# %%
+l3_merged.z_surf_combined.plot(marker='o')
